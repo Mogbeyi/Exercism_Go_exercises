cb525f0eb829dbae4811b6e8b8790b2b97e0d366
Mogbeyi <mogbeyiwenyinmi@gmail.com>
2022-06-21 10:07:37 +0100

    Solve annalyns infiltration challenge

---
+++ annalyns-infiltration/annalyns_infiltration.go
@@ -0,0 +1,29 @@
+package annalyn
+
+func CanFastAttack(knightIsAwake bool) bool {
+	return !knightIsAwake
+}
+
+func CanSpy(knightIsAwake, archerIsAwake, prisonerIsAwake bool) bool {
+	if knightIsAwake || archerIsAwake || prisonerIsAwake {
+		return true
+	}
+	return false
+}
+
+func CanSignalPrisoner(archerIsAwake, prisonerIsAwake bool) bool {
+	if prisonerIsAwake && !archerIsAwake {
+		return true
+	}
+	return false
+}
+
+func CanFreePrisoner(knightIsAwake, archerIsAwake, prisonerIsAwake, petDogIsPresent bool) bool {
+	if prisonerIsAwake && (archerIsAwake == false && !knightIsAwake) {
+		return true
+	} else if petDogIsPresent && !archerIsAwake {
+		return true
+	} else {
+		return false
+	}
+}
