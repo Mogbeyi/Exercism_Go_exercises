74116093807f7e56c36b92df9556f747d32f9db6
Mogbeyi <mogbeyiwenyinmi@gmail.com>
2022-06-21 10:22:41 +0100

    Use concise booleans to solve problems

--- annalyns-infiltration/annalyns_infiltration.go
+++ annalyns-infiltration/annalyns_infiltration.go
@@ -5,25 +5,27 @@ func CanFastAttack(knightIsAwake bool) bool {
 }
 
 func CanSpy(knightIsAwake, archerIsAwake, prisonerIsAwake bool) bool {
-	if knightIsAwake || archerIsAwake || prisonerIsAwake {
-		return true
-	}
-	return false
+	return knightIsAwake || archerIsAwake || prisonerIsAwake 
 }
 
 func CanSignalPrisoner(archerIsAwake, prisonerIsAwake bool) bool {
-	if prisonerIsAwake && !archerIsAwake {
-		return true
-	}
-	return false
+	return prisonerIsAwake && !archerIsAwake
 }
 
-func CanFreePrisoner(knightIsAwake, archerIsAwake, prisonerIsAwake, petDogIsPresent bool) bool {
-	if prisonerIsAwake && (!archerIsAwake && !knightIsAwake) {
-		return true
-	} else if petDogIsPresent && !archerIsAwake {
-		return true
-	} else {
-		return false
-	}
+// func canfreeprisoner(knightisawake, archerisawake, prisonerisawake, petdogispresent bool) bool {
+// 	if prisonerisawake && (!archerisawake && !knightisawake) {
+// 		return true
+// 	} else if petdogispresent && !archerisawake {
+// 		return true
+// 	} else {
+// 		return false
+// 	}
+// }
+
+
+// CanFreePrisoner can be executed if prisoner is awake and the other 3 characters are asleep
+// or if Annalyn's pet dog is with her and the archer is sleeping
+func CanFreePrisoner(knightIsAwake, archerIsAwake, prisonerIsAwake, petdogispresent bool) bool {
+	return (prisonerIsAwake && !archerIsAwake && !knightIsAwake && !petdogispresent) || (petdogispresent && !archerIsAwake)
 }
+
