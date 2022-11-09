41d12395d74baa5bf2c27121b2eff41a2905d004
Emmy Wonder <emmywonder@Emmys-MacBook-Pro.local>
2022-11-09 13:41:34 +0100

    Final black jack solution

--- blackjack/blackjack.go
+++ blackjack/blackjack.go
@@ -1,10 +1,7 @@
 package blackjack
-
 // ParseCard returns the integer value of a card following blackjack ruleset.
 func ParseCard(card string) int {
 	switch card {
-	case "ten", "jack", "queen", "king":
-		return 10
 	case "ace":
 		return 11
 	case "two":
@@ -23,36 +20,57 @@ func ParseCard(card string) int {
 		return 8
 	case "nine":
 		return 9
+	case "ten", "jack", "queen", "king":
+		return 10
+	default:
+		return 0
 	}
-
-	return 0
+	/*
+	   ace		11	eight	8
+	   two		2	nine	9
+	   three	3	ten		10
+	   four	4	jack	10
+	   five	5	queen	10
+	   six		6	king	10
+	   seven	7	other	0
+	*/
 }
-
 // FirstTurn returns the decision for the first turn, given two cards of the
 // player and one card of the dealer.
 func FirstTurn(card1, card2, dealerCard string) string {
-	card1Value := ParseCard(card1)
-	card2Value := ParseCard(card2)
-	dealerCardValue := ParseCard(dealerCard)
-	cardSum := card1Value + card2Value
-
+	var response string
+	var dealerCardValue int = ParseCard(dealerCard)
+	var myCardSum int = ParseCard(card1) + ParseCard(card2)
+	/*
+ 
+	   Stand (S)
+	   Hit (H)
+	   Split (P)
+	   Automatically win (W)
+	   Although not optimal yet, you will follow the strategy your friend Alex has been developing, which is as follows:
+ 
+	   If you have a pair of aces you must always split them.
+	   If you have a Blackjack (two cards that sum up to a value of 21), and the dealer does not have an ace, a figure or a ten then you automatically win. If the dealer does have any of those cards then you'll have to stand and wait for the reveal of the other card.
+	   If your cards sum up to a value within the range [17, 20] you should always stand.
+	   If your cards sum up to a value within the range [12, 16] you should always stand unless the dealer has a 7 or higher, in which case you should always hit.
+	   If your cards sum up to 11 or lower you should always hit.
+ 
+	*/
 	switch {
-	case card1 == "ace" && card2 == "ace":
-		return "P"
-	case cardSum == 21:
-		if dealerCard != "ace"  || dealerCardValue != 0 || dealerCard != "ten"{
-			return "W"
-		}
-		return "S"
-	case cardSum == 17 || cardSum == 18 || cardSum == 19 || cardSum == 20:
-		return "S"
-	case cardSum == 12 || cardSum == 13 || cardSum == 14 || cardSum == 15 || cardSum == 16:
-		if dealerCardValue >= 7 {
-			return "H"
-		}
-		return "S"
-	case cardSum <= 11:
-		return "H"
+	case ParseCard(card1) == 11 && ParseCard(card2) == 11:
+		response = "P"
+	case myCardSum == 21 && (dealerCardValue != 11 && dealerCardValue != 10):
+		response = "W"
+	case (myCardSum == 21) && (dealerCardValue == 11 || dealerCardValue == 10):
+		response = "S"
+	case (myCardSum >= 17 && myCardSum <= 20):
+		response = "S"
+	case (myCardSum >= 12 && myCardSum <= 16) && (dealerCardValue < 7):
+		response = "S"
+	case (myCardSum >= 12 && myCardSum <= 16) && (dealerCardValue >= 7):
+		response = "H"
+	case (myCardSum <= 11):
+		response = "H"
 	}
-	return ""
+	return response
 }
