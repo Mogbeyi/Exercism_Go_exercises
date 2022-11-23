4030cf3dc9586331ec8d4242bf1d9014b57928ec
Emmy Wonder <emmywonder@Emmys-MacBook-Pro.local>
2022-11-23 10:08:34 +0100

    Make minor changes

--- gross-store/gross_store.go
+++ gross-store/gross_store.go
@@ -10,10 +10,12 @@ func Units() map[string]int {
 		"great_gross": 1728,
 	}
 }
+
 // NewBill create a new bill
 func NewBill() map[string]int {
 	return map[string]int{}
 }
+
 // AddItem add item to customer bill
 func AddItem(bill, units map[string]int, item, unit string) bool {
 	value, exists := units[unit]
@@ -23,6 +25,7 @@ func AddItem(bill, units map[string]int, item, unit string) bool {
 	}
 	return false
 }
+
 // RemoveItem remove item from customer bill
 func RemoveItem(bill, units map[string]int, item, unit string) bool {
 	value, exists := units[unit]
@@ -38,6 +41,7 @@ func RemoveItem(bill, units map[string]int, item, unit string) bool {
 	}
 	return false
 }
+
 // GetItem return the quantity of item that the customer has in his/her bill
 func GetItem(bill map[string]int, item string) (int, bool) {
 	qty, exist := bill[item]
